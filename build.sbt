a517c789e0021cf198bccf6c9ab99845aebde5a7
Will Sargent <will.sargent@lightbend.com>
2017-03-24 14:45:33 -0700

    Update with test against async result

--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ libraryDependencies += javaJpa
 libraryDependencies += "org.hibernate" % "hibernate-core" % "5.2.5.Final"
 
 libraryDependencies += javaWs % "test"
+
+libraryDependencies += "org.awaitility" % "awaitility" % "2.0.0" % "test"
+libraryDependencies += "org.assertj" % "assertj-core" % "3.6.2" % "test"
 libraryDependencies += "org.mockito" % "mockito-core" % "2.1.0" % "test"
 testOptions in Test += Tests.Argument(TestFrameworks.JUnit, "-a", "-v")
--- test/UnitTest.java
+++ test/UnitTest.java
@@ -1,18 +1,31 @@
 import controllers.PersonController;
+import models.Person;
 import models.PersonRepository;
 import org.junit.Test;
 import play.data.FormFactory;
+import play.data.format.Formatters;
+import play.i18n.MessagesApi;
+import play.libs.Json;
 import play.libs.concurrent.HttpExecutionContext;
+import play.mvc.Http;
 import play.mvc.Result;
 import play.twirl.api.Content;
 
+import javax.validation.Validator;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ForkJoinPool;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.*;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static play.mvc.Http.Status.OK;
+import static play.mvc.Http.Status.SEE_OTHER;
 import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.invokeWithContext;
 
 /**
  * Simple (JUnit) tests that can call all parts of a play app.
@@ -29,13 +42,48 @@ public class UnitTest {
         final PersonController controller = new PersonController(formFactory, repository, ec);
         final Result result = controller.index();
 
-        assertEquals(OK, result.status());
+        assertThat(result.status()).isEqualTo(OK);
     }
 
     @Test
     public void checkTemplate() {
         Content html = views.html.index.render();
-        assertEquals("text/html", html.contentType());
-        assertTrue(contentAsString(html).contains("Add Person"));
+        assertThat(html.contentType()).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("Add Person");
     }
+
+    @Test
+    public void checkAddPerson() {
+        // Easier to mock out the form factory inputs here
+        MessagesApi messagesApi = mock(MessagesApi.class);
+        Validator validator = mock(Validator.class);
+        FormFactory formFactory = new FormFactory(messagesApi, new Formatters(messagesApi), validator);
+
+        // Don't need to be this involved in setting up the mock, but for demo it works:
+        PersonRepository repository = mock(PersonRepository.class);
+        Person person = new Person();
+        person.id = 1L;
+        person.name = "Steve";
+        when(repository.add(any())).thenReturn(supplyAsync(() -> person));
+
+        // Set up the request builder to reflect input
+        final Http.RequestBuilder requestBuilder = new Http.RequestBuilder().method("post").bodyJson(Json.toJson(person));
+
+        // Add in an Http.Context here using invokeWithContext:
+        final CompletionStage<Result> stage = invokeWithContext(requestBuilder, () -> {
+            HttpExecutionContext ec = new HttpExecutionContext(ForkJoinPool.commonPool());
+
+            // Create controller and call method under test:
+            final PersonController controller = new PersonController(formFactory, repository, ec);
+            return controller.addPerson();
+        });
+
+        // Test the completed result
+        await().atMost(1, SECONDS).until(() ->
+            assertThat(stage.toCompletableFuture()).isCompletedWithValueMatching(result ->
+                result.status() == SEE_OTHER, "Should redirect after operation"
+            )
+        );
+    }
+
 }
